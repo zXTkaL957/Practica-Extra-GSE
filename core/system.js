59054e2b3e4a4f1ea0b5702abb9d8f0f70e42a53
zXTkaL957 <gomez.santiago.eduardo2@gmail.com>
2022-12-11 09:22:47 -0600

    first commit

---
+++ core/system.js
@@ -0,0 +1,9 @@
+const si = require('systeminformation');
+
+exports.cpu = async () => si.cpu();
+
+exports.cpuCurrentspeed = async () => si.cpuCurrentspeed();
+
+exports.mem = async () => si.mem();
+
+
